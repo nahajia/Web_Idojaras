e00d2fd0d8e196a1282446bedc02f0cc2c722e64
Aniko <nahajia@gmail.com>
2025-04-14 09:07:57 +0200

    töltő kép beillesztése

--- alap.js
+++ alap.js
@@ -1,5 +1,8 @@
 //47.53333 21.63333
 //47.53333 21.644821
+document.getElementById("koordinatak").innerHTML=`
+<img src="toltes.gif" >
+`
 
 let latitude = 47.53333
 let longitude = 21.63333
@@ -7,6 +10,9 @@ let longitude = 21.63333
 fetchFuggveny()
 
 function keres() {
+    document.getElementById("koordinatak").innerHTML=`
+<img src="toltes.gif" >
+`
     let ujLat = document.getElementById("lat").value
     let ujLong = document.getElementById("long").value
     latitude = ujLat
